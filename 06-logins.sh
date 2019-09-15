9985bccaa36033e365ec33f2cec9c8800653be0b
Jonathan Backhaus <jonathan.david.backhaus@gmail.com>
2019-09-15 17:01:08 -0400

    Aligning to local (offline) ODROID directory

--- 06-logins.sh
+++ 06-logins.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # source $DIR/config/colors.txt
+. ${DIR}/config/colors.txt
 
 echo -e " ${YEL}SSH Logins..........:${NC} There are currently ${PUR}$(who | wc -l)${NC} users logged in"
